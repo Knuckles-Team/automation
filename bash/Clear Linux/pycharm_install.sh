bc3c362277b4fc31e2138bf06b453debf3537bfe
Audel <knucklessg1@gmail.com>
2020-07-18 13:15:30 -0500

    initial files

---
+++ bash/Clear Linux/pycharm_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo swupd bundle-add desktop
+sudo swupd bundle-add python3-tcl
+sudo swupd bundle-add c-basic
+sudo pip install ./pywinpty-0.5.7-cp39-cp39-win_amd64.whl
+flatpak install flathub com.jetbrains.PyCharm-Community
+flatpak run com.jetbrains.PyCharm-Community
