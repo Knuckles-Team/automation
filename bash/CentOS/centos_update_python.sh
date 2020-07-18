f48575a6540c5bbeb514e6ed79c47acda376ea0c
Audel <knucklessg1@gmail.com>
2020-07-18 14:14:01 -0500

    Cleaned up again

--- bash/CentOS/centos_update_python.sh
+++ bash/CentOS/centos_update_python.sh
@@ -9,5 +9,5 @@ sudo python3.8 -m pip install --upgrade pip
 # Install Python Depedencies
 sudo yum install gcc python3-devel python38-devel openssl-devel tcl-thread xz-libs bzip2-devel libffi-devel python3-tkinter python38-tkinter -y 
 # Update all Packages
-sudo python3 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-sudo python3.8 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+sudo python3 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H python3 -m pip install -U
+sudo python3.8 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H python3.8 -m pip install -U
