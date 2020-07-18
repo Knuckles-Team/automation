6037f1842f4713f0114ad00635af7daee2875b42
Audel <knucklessg1@gmail.com>
2020-07-18 16:13:35 -0500

    cleaned repo

--- bash/CentOS/centos_run_all_scripts.sh
+++ bash/CentOS/centos_run_all_scripts.sh
@@ -5,11 +5,15 @@
 sudo ./centos_enable_storage.sh
 # Install Latest Chrome
 sudo ./centos_chrome_install.sh
+# Install FFmpeg
+sudo ./centos_ffmpeg_install.sh
 # Install Python Developer Environment with PyCharm + Dependencies 
 sudo ./centos_python_env_install.sh
 # Install + Configures KVM
 sudo ./centos_kvm_install.sh
 # Install Gparted
 sudo ./centos_gparted_install.sh
+# Update FFmpeg
+sudo ./centos_update_ffmpeg.sh
 # Updates all Python Packages
 sudo ./centos_update_python.sh
