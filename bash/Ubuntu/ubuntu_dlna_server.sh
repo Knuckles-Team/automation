ab7ca9d08d8a2f2f79faa5ece142ae07448a9592
knucklessg1@gmail.com <knucklessg1@gmail.com>
2020-11-10 20:32:12 -0600

    Added whoami to dlna server script for Ubuntu

--- bash/Ubuntu/ubuntu_dlna_server.sh
+++ bash/Ubuntu/ubuntu_dlna_server.sh
@@ -3,6 +3,9 @@
 sudo apt update
 sudo apt install rygel -y
 # edit /etc/rygel.conf based off man rygel.conf
-echo "uris=/mnt/w/Movies;/mnt/z/Music" | sudo tee -a /etc/rygel.conf
-
+# For WSL2:
+#echo "uris=/mnt/w/Movies;/mnt/z/Music" | sudo tee -a /etc/rygel.conf
+# For Ubuntu
+user=$(whoami)
+echo "uris=/media/${user}/Movies/Movies" | sudo tee -a /etc/rygel.conf
 #sed -i '' ~/.config/rygel.conf
