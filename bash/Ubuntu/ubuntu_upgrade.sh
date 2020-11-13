66ca2f14562a8552812b149abe8a211a039b3283
Knucklessg1 <knucklessg1@gmail.com>
2020-11-13 00:47:27 -0600

    Added ubuntu upgrade script.

---
+++ bash/Ubuntu/ubuntu_upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Updating packages from repositories.
+sudo apt update
+# Upgrading Packages.
+sudo apt dist-upgrade -y
+
+# House Cleaning
+# The first line will remove/fix any residual/broken packages if any.
+sudo apt --purge autoremove
+# The clean command removes all old .deb files from the apt cache (/var/cache/apt/archives)
+sudo apt clean all
+# Removes package configurations left over from packages that have been removed (but not purged).
+sudo apt purge $(dpkg -l | awk '/^rc/ { print $2 }')
+
+# Upgrading OS
+sudo do-release-upgrade
