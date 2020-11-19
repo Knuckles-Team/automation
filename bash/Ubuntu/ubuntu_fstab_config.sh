de6ec601a8cc42aaa0655d4df7060342b0ad6ba4
knucklessg1@gmail.com <knucklessg1@gmail.com>
2020-11-18 19:12:44 -0600

    Fixed transmission

--- bash/Ubuntu/ubuntu_fstab_config.sh
+++ bash/Ubuntu/ubuntu_fstab_config.sh
@@ -9,10 +9,10 @@ sudo mkdir /media/mrdr/windows
 sudo mkdir /media/mrdr/movies
 sudo mkdir /media/mrdr/games
 
-echo "/dev/sda1 /media/mrdr/hdd_storage ntfs-3g rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime,nofail,nodev,nosuid 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb2 /media/mrdr/file_storage ntfs-3g rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime,nofail,nodev,nosuid 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc4 /media/mrdr/windows ntfs-3g rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime,nofail,nodev,nosuid 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sde2 /media/mrdr/movies ntfs-3g rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime,nofail,nodev,nosuid 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdf2 /media/mrdr/games ntfs-3g rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime,nofail,nodev,nosuid 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sda1 /media/mrdr/hdd_storage ntfs-3g rw,auto,user,permissions,uid=1000,gid=1000,umask=0000,noatime,nodiratime,nofail,nodev,nosuid,exec 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdb2 /media/mrdr/file_storage ntfs-3g rw,auto,user,permissions,uid=1000,gid=1000,umask=0000,noatime,nodiratime,nofail,nodev,nosuid,exec 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc4 /media/mrdr/windows ntfs-3g rw,auto,user,permissions,uid=1000,gid=1000,umask=0000,noatime,nodiratime,nofail,nodev,nosuid,exec 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sde2 /media/mrdr/movies ntfs-3g rw,auto,user,permissions,uid=1000,gid=1000,umask=0000,noatime,nodiratime,nofail,nodev,nosuid,exec 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdf2 /media/mrdr/games ntfs-3g rw,auto,user,permissions,uid=1000,gid=1000,umask=0000,noatime,nodiratime,nofail,nodev,nosuid,exec 0 0" | sudo tee -a /etc/fstab
 
 sudo mount -a
--- bash/Ubuntu/ubuntu_transmission.sh
+++ bash/Ubuntu/ubuntu_transmission.sh
@@ -10,11 +10,15 @@ function usage() {
   echo "Add Links [Magnet or Torrent] or File Locations"
   echo "sudo ./ubuntu_transmission.sh add <link>"
   echo "Remove Torrent"
-  echo "sudo ./ubuntu_transmission.sh remove <#> (Run the status command to see the Torrent IDs)"
+  echo "sudo ./ubuntu_transmission.sh remove 1,4,6,9 (Run the status command to see the Torrent IDs)"
+  echo "Remove Completed Torrent"
+  echo "sudo ./ubuntu_transmission.sh remove_complete"
   echo "Change Download Directory"
-  echo "sudo ./ubuntu_transmission.sh set_download_path"
+  echo "sudo ./ubuntu_transmission.sh set_download_path <fullpath_download_directory> <optional_incomplete_download_directory>"
   echo "Download Status"
   echo "sudo ./ubuntu_transmission.sh status"
+  echo "Constant Status Update"
+  echo "watch ./ubuntu_transmission.sh status"
   echo "Stop Daemon"
   echo "sudo ./ubuntu_transmission.sh stop"
 }
@@ -24,57 +28,81 @@ function install() {
   echo "Installing Transmission"
   sudo apt update
   sudo apt install transmission-qt transmission-cli transmission-daemon -y
-  mkdir ~/Torrents/ || echo "Directory ~/Torrents/ already exists"
+  mkdir /home/${USER}/Torrents/ || echo "Directory /home/${USER}/Torrents/ already exists"
   #transmission-daemon --download-dir ~/Torrents/
-  /etc/init.d/transmission-daemon stop
+  sudo /etc/init.d/transmission-daemon stop
   sudo sed -i "s/\"download-dir\": \"\/var\/lib\/transmission-daemon\/downloads\"/\"download-dir\": \"\/media\/${USER}\/movies\/Torrents\/Complete\"/" /etc/transmission-daemon/settings.json
   sudo sed -i "s/\"incomplete-dir\": \"\/var\/lib\/transmission-daemon\/Downloads\"/\"incomplete-dir\": \"\/media\/${USER}\/movies\/Torrents\/Downloading\"/" /etc/transmission-daemon/settings.json
   sudo sed -i 's/"rpc-authentication-required": true/"rpc-authentication-required": false/' /etc/transmission-daemon/settings.json
   sudo mkdir -p /etc/systemd/system/transmission-daemon.service.d/
   printf "[Service]\nEnvironment=TRANSMISSION_HOME=/etc/transmission-daemon" | sudo tee -a /etc/systemd/system/transmission-daemon.service.d/override.conf
-  /etc/init.d/transmission-daemon restart
+  sudo transmission-remote -w "/home/${USER}/Torrents"
+  # Add User to the debian-transmission group
+  sudo usermod -a -G debian-transmission ${USER}
+  # Change the folder ownership
+  sudo chgrp debian-transmission /home/${USER}/Torrents
+  # Grant write access to the group
+  sudo chmod 770 /home/${USER}/Torrents
+  sudo service transmission-daemon stop
+  sudo sed -i 's/"umask": 18/"umask": 2/' /etc/transmission-daemon/settings.json
+  sudo /etc/init.d/transmission-daemon restart
   sudo systemctl daemon-reload
-  transmission-remote -l
+  sudo adduser debian-transmission users
+  sudo service transmission-daemon start
+  sudo transmission-remote -l
 }
 
 # Set Download Path
 function set_download_path() {
-  /etc/init.d/transmission-daemon stop
-  sudo sed -i "s/\"download-dir\": \"\/var\/lib\/transmission-daemon\/downloads\"/\"download-dir\": ${1}/" /etc/transmission-daemon/settings.json
-  sudo sed -i "s/\"incomplete-dir\": \"\/var\/lib\/transmission-daemon\/Downloads\"/\"incomplete-dir\": ${1}/" /etc/transmission-daemon/settings.json
-  sudo sed -i 's/"rpc-authentication-required": true/"rpc-authentication-required": false/' /etc/transmission-daemon/settings.json
-  /etc/init.d/transmission-daemon restart
+  if [[ "${#download_args[@]}" -le 1 ]] ; then    
+    incomplete_dir=$1   
+  else
+    incomplete_dir=$2  
+  fi
+  download_dir=$1
+  echo "Setting download directory ${download_dir}"
+  #sudo /etc/init.d/transmission-daemon stop || echo "Already Stopped"
+  sudo sed -i "s#download-dir[\"].*#download-dir\": \"${download_dir}\",#" /etc/transmission-daemon/settings.json
+  sudo sed -i "s#incomplete-dir[\"].*#incomplete-dir\": \"${incomplete_dir}\",#" /etc/transmission-daemon/settings.json
+  sudo transmission-remote -w "${download_dir}"
+  echo 'Successfully updated download directory/(ies)'
+  sudo /etc/init.d/transmission-daemon restart
   sudo systemctl daemon-reload
-  transmission-remote -l
+  sudo transmission-remote -l
 }
 
 # Load Transmission Daemon
 function load() {
-  transmission-daemon
+  sudo transmission-daemon
 }
 
 # Stop Transmission Daemon
 function stop() {
-  transmission-remote --exit
+  sudo transmission-remote --exit
   killall transmission-daemon
 }
 
 # Add Torrent
 function add() {
-  load
   echo "Adding $1"
-  transmission-remote -a "${1}"
+  sudo transmission-remote -a "${1}"
 }
 
 # Remove Torrent
 function remove() {
   echo "Removing $1"
-  transmission-remote -t "${1}" -r
+  sudo transmission-remote -t "${1}" -r
+}
+
+# Remove Torrent
+function remove_complete() {
+  echo "Removing Completed Torrents"
+  sudo transmission-remote -t "${1}" -r
 }
 
 # Check Status of Torrents
 function status() {
-  transmission-remote -l
+  sudo transmission-remote -l
 }
 
 # Main Function
@@ -90,12 +118,16 @@ function main() {
     add "${args[1]}"
   elif [[ ${args[0]} == "remove" ]] ; then
     remove "${args[1]}"
+  elif [[ ${args[0]} == "remove_complete" ]] ; then
+    remove_complete
   elif [[ ${args[0]} == "status" ]] ; then
     status
   elif [[ ${args[0]} == "stop" ]] ; then
     stop
   elif [[ ${args[0]} == "set_download_path" ]] ; then
-    set_download_path
+    set_download_path "${args[1]}" "${args[2]}"
+  else
+    usage
   fi
 }
 
