1985c1f1fd816214b552c62453deee291bf14b66
knucklessg1@gmail.com <knucklessg1@gmail.com>
2021-01-31 17:02:36 -0600

    Added video downloader and cleaned up provisioner

--- bash/provision_system.sh
+++ bash/provision_system.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 
-function loading_message(){
-	# Usage: loading_message <total> <iteration>
-	additional_message="${3}"
-	if [[ "${additional_message}" ]]; then
-    additional_message="| ${additional_message}"
-  fi
-	total_left=$( echo "${1}-${2}" | bc -l )
-	percent_left=$(echo "${2}/${1}*100" | bc -l)
-	percent_left=$(echo "${percent_left}" | cut -c1-5)
-	loading_bar_percentage=$(( $(printf "%.*f\n" "0" "${percent_left}")/10 ))
-	loading_bar=""
-	for ((x=1;x<=10;x++))
-	do
-		if [[ "${x}" -le "${loading_bar_percentage}" ]]; then
-			loading_bar="${loading_bar}#"
-		else
-			loading_bar="${loading_bar} "
-		fi
-	done
-	echo -ne "Total Remaining: ${total_left} | Ratio: ${2}/${1} | Percent Complete: ${percent_left} | [${loading_bar}] ${additional_message}\r"
-}
-
 function provision(){
-  #set -x
-  provision_log="./provision_log.txt"
   readarray -d '' install_scripts < <(find . -name "*_install.sh" -print0)
   readarray -d '' upgrade_scripts < <(find . -name "os_upgrade.sh" -print0)
   run_scripts=( "${upgrade_scripts[@]}" "${install_scripts[@]}" )
   echo "Upgrade OS & Installing all applications"
   for ((script=0;script<${#run_scripts[*]};script++))
   do
-    #loading_message "${run_scripts[script]}" "$((script+1))" "Running ${script}"
     echo "Running ${script}"
-    sudo "${run_scripts[script]}" #>> "${provision_log}"
+    sudo "${run_scripts[script]}"
   done
   echo "System Provisioned Successfully"
 }
---
+++ bash/video_download.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# This script will retitle all .mkv/.mp4 metadata to their file names. Will also rename directories to the file name
+function usage() {
+  echo -e "Flags:\n"
+  echo -e "-i | --install [Install dependencies]\n-d | --download_directory [Set the download directory]\n-f | --file <FILE> [Read links from textfile]\n-l | --links <LINK1, LINK2, LINK3> [Read links from command prompt]\n-a | --audio [Downloads audio only]"
+  echo -e "\nUsage: \nInstall: "
+  echo "sudo ./video_download.sh -i"
+  echo "sudo ./video_download.sh --install"
+  echo -e "\nDownload with file: "
+  echo "./video_download.sh -f <FILE>"
+  echo "./video_download.sh --file <FILE>"
+  echo -e "\nDownload with links: "
+  echo "./video_download.sh -l <LINK1,LINK2,LINK3>"
+  echo "./video_download.sh -l <LINK1, LINK2, LINK3>"
+  echo "./video_download.sh --links <LINK1,LINK2,LINK3>"
+  echo "./video_download.sh --links <LINK1, LINK2, LINK3>"
+  echo -e "\nSet Download Directory: "
+  echo './video_download.sh -f <FILE> -d "~/Downloads"'
+}
+
+function detect_os(){
+  os_version=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+	os_version="${os_version:1:-1}"
+	echo "${os_version}"
+	if [[ $os_version = "Ubuntu" ]] ; then
+		echo "Installing for Ubuntu"		
+		ubuntu_install
+  elif [[ $os_version = "CentOS Linux" ]] ; then
+		echo "Installing for CentOS"
+		centos_install
+  else 
+    echo "Distribution ${os_version} not supported"
+	fi
+}
+
+function ubuntu_install(){
+	sudo apt update
+  sudo apt install -y youtube-dl
+}
+
+function centos_install(){
+	sudo yum update -y
+	sudo yum install youtube-dl -y
+}
+
+function download(){
+  echo "Beginning downloading"
+  for link in "${links[@]}"
+  do
+    if [[ -n $( echo "${link}" | grep 'https://rumble.com' ) ]]; then
+      echo "Downloading Rumble Video: ${link}"
+      if [[ "${audio_flag}" == "true" ]]; then
+       youtube-dl -x --audio-format mp3 --no-check-certificate -o "${download_dir}/%(title)s.%(ext)s" -f best/bestaudio "$(curl -s "${link}" | tr -d '\n'|awk -F "embedUrl" '{print $2}'|awk -F '"' '{print $3}')";
+      else
+        youtube-dl --no-check-certificate -o "${download_dir}/%(title)s.%(ext)s" -f mp4-1080p/mp4-720p/mp4-480p/webm-480p/mp4-360p/ "$(curl -s "${link}" | tr -d '\n'|awk -F "embedUrl" '{print $2}'|awk -F '"' '{print $3}')";
+      fi
+    elif [[ -n $( echo "${link}" | grep 'https://youtube.com' ) ]] || [[ -n $( echo "${link}" | grep 'https://www.youtube.com' ) ]] ; then
+      echo "Downloading YouTube Video: ${link}"
+      if [[ "${audio_flag}" == "true" ]]; then
+        youtube-dl -x --audio-format mp3 -f best/bestaudio --write-description --write-info-json --write-annotations --write-sub --write-thumbnail --no-check-certificate -o "${download_dir}/%(title)s.%(ext)s" "${link}"
+      else
+        youtube-dl -f best --no-check-certificate -o "${download_dir}/%(title)s.%(ext)s" "${link}"
+      fi
+    else
+      echo "ERROR: Not a valid link: ${link}"
+    fi
+  done
+}
+
+audio_flag='false'
+download_dir="~/Downloads"
+while test -n "$1"; do
+  case "$1" in
+    a | -a | --audio)
+      audio_flag='true'
+      shift
+      ;;
+    h | -h | --help)
+      usage
+      exit 0
+      ;;
+    i | -i | --install)
+      echo "Installing"
+      detect_os
+      exit 0
+      ;;
+    d | -d | --download-directory)
+      if [ ${2} ]; then
+        download_dir="${2}"
+        shift
+      else
+        echo 'ERROR: "-d | --download-directory" requires a non-empty option argument.'
+        exit 0
+      fi
+      ;;
+    l | -l | --links)
+      echo -e "1: $1\n2: $2"
+      if [ ${2} ]; then
+        IFS=',' read -r -a links_direct2 <<< "$2"
+        links=( "${links_direct1[@]}" "${links_direct2[@]}" )
+        echo "LINKS: ${links[*]}"
+        shift
+      else
+        echo 'ERROR: "-l | --links" requires a non-empty option argument.'
+        exit 0
+      fi
+      ;;
+    f | -f | --file)
+      if [ ${2} ]; then
+        file=${2}
+        IFS=$'\n' read -d '' -r -a file_links < "${file}"
+        links=( "${links[@]}" "${file_links[@]}" )
+        shift
+      else
+        echo 'ERROR: "-f | --file" requires a non-empty option argument.'
+        exit 0
+      fi
+      ;;
+    --)              # End of all options.
+      shift
+      break
+      ;;
+    -?*)
+      printf 'WARNING: Unknown option (ignored): %s\n' "$1" >&2
+      ;;
+    *)
+      shift
+      break
+      ;;
+  esac
+  shift
+done
+
+download
