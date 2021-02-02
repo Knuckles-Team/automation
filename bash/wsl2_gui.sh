d7393561a9574f8121dedce108025b5f13083788
knucklessg1@gmail.com <knucklessg1@gmail.com>
2021-02-01 20:53:02 -0600

    Testing Ubuntu GUI for Windows

---
+++ bash/wsl2_gui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+sudo apt update && sudo apt -y upgrade
+# Remove previous xrdp
+sudo apt purge xrdp
+
+# install xfce
+sudo apt-get install -y xfce4 xfce4-goodies
+
+# install xrdp
+sudo apt-get install xrdp
+
+# Set Port and other settings
+echo -e "#xce4\nStartxfce4" | sudo tee -a /etc/xrdp/startwm.sh
+sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.iniecho xfce4-session | tee ~/.xsession#enable dbus
+sudo systemctl enable dbus
+sudo /etc/init.d/dbus start
+sudo /etc/init.d/xrdp start
