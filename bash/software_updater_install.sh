dc238739d76b694f451efd1c0793fc075b634aee
knucklessg1@gmail.com <knucklessg1@gmail.com>
2021-02-11 01:14:45 -0600

    Added ubuntu software updater.

---
+++ bash/software_updater_install.sh
@@ -0,0 +1,29 @@
+function detect_os(){
+  os_version=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+	os_version="${os_version:1:-1}"
+	echo "${os_version}"
+	if [[ $os_version = "Ubuntu" ]] ; then
+		echo "Installing for Ubuntu"
+		ubuntu_install
+  elif [[ $os_version = "CentOS Linux" ]] ; then
+		echo "Installing for CentOS"
+		centos_install
+  else
+    echo "Distribution ${os_version} not supported"
+	fi
+}
+
+function ubuntu_install(){
+	sudo apt update
+	sudo apt install update-manager synaptic -y
+}
+
+function centos_install(){
+	echo "For Ubuntu Only, not compatible with CentOS"
+}
+
+function main(){
+  detect_os
+}
+
+main
