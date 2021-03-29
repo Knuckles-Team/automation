1bb1abfb43d0658ee5bc9fb9f8d9ccc0da36e0df
Knucklessg1 <knucklessg1@gmail.com>
2021-03-29 02:05:50 -0500

    audacity is fulled automated v2.

--- bash/video_rename.sh
+++ bash/video_rename.sh
@@ -44,11 +44,11 @@ function install_dependencies() {
 function file_rename() {
   list=$1
   file_type=$2
-  if [[ ! -z "${list}" ]]
+  if [[ -n "${list}" ]]
   then
     for file in "${list[@]}"
     do
-      printf "Checking ${file_type}: ${file}\n"
+      #printf "Checking ${file_type}: ${file}\n"
       x="${file}"
       if [[ "${file_type}" == "mkv" ]]
         then
@@ -62,63 +62,63 @@ function file_rename() {
       fi      
       title=${y##*/}
       current_title=$(mediainfo "${file}" | grep -e "Movie name" | awk -F  ":" '{print $2}' | sed 's/^ *//')
-      printf "Current Title: ${current_title}\nProposed Title: ${title}\n"
+      #printf "Current Title: ${current_title}\nProposed Title: ${title}\n"
       if [[ "${title}" != "${current_title}" ]]
       then
         if [[ "${file_type}" == "mkv" ]]
         then
-        mkvpropedit "${file}" -e info -s title="${title}"
-        printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
+        mkvpropedit "${file}" -e info -s title="${title}" > /dev/null 2>&1
+        #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
       elif [[ "${file_type}" == "webm" ]]
       then
-        mkvpropedit "${file}" -e info -s title="${title}"
-        printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
+        mkvpropedit "${file}" -e info -s title="${title}" > /dev/null 2>&1
+        #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
       elif [[ "${file_type}" == "mp4" ]]
       then
-        AtomicParsley "${file}" --title "${title}" --comment "" --overWrite
-        printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
+        AtomicParsley "${file}" --title "${title}" --comment "" --overWrite > /dev/null 2>&1
+        #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
       fi
-      else
-        printf "Titles already the same, no need to update: ${file}\n"
+#      else
+#        printf "Titles already the same, no need to update: ${file}\n"
       fi    
       # Rename Directory of Folder
       rename_directory "${directory}" "${title}"
     done
-  else
-    printf "Found no ${file_type} Files in ${directory}\n"
   fi
 }
 
 function find_files() {
+  count=0
   for directory in "${directories[@]}"
   do
     mp4_list=("${directory}"/*.mp4)
     mkv_list=("${directory}"/*.mkv)
     webm_list=("${directory}"/*.webm)
-    if [[ ! -z "${mkv_list}" ]]
+    percent_complete=$(((count/${#directories[@]})*100))
+    if [[ -n "${mkv_list}" ]]
     then
-      echo "Processing MKV Files"
+      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Processing MKV Files \r"
       file_rename "${mkv_list}" "mkv"
     else
-      printf "Found no mkv Files in ${directory}\n"
+      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Found no MKV Files in ${directory} \r"
     fi
-    sleep 1
-    if [[ ! -z "${mp4_list}" ]]
+    if [[ -n "${mp4_list}" ]]
     then
-      echo "Processing MP4 Files"
+      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Processing MP4 Files \r"
       file_rename "${mp4_list}" "mp4"
     else
-      printf "Found no mp4 Files in ${directory}\n"
+      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Found no MP4 Files in ${directory} \r"
     fi
-    sleep 1
-    if [[ ! -z "${webm_list}" ]]
+    if [[ -n "${webm_list}" ]]
     then
-      echo "Processing WEBM Files"
+      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Processing WEBM Files \r"
       file_rename "${webm_list}" "webm"
     else
-      printf "Found no webm Files in ${directory}\n"
+      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Found no WEBM Files in ${directory} \r"
     fi
-    sleep 1
+    ((count++))
+    percent_complete=$(((count/${#directories[@]})*100))
+    echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Completed File Renaming \r"
   done
 }
 
@@ -129,30 +129,30 @@ function find_directories() {
   
   while read line
   do
-    printf "LINE: ${line}\n"
+    #printf "LINE: ${line}\n"
     if [ -d "${line}" ]
     then
       directories[ $i ]="${line}" 
-      echo "Found Valid Directory: ${directories[i]} Count: ${i}"       
+      #echo "Found Valid Directory: ${directories[i]} Count: ${i}"
       (( i++ ))
-    else
-      echo "Did not find a directory at: ${line} Count: ${i}"
+    #else
+      #echo "Did not find a directory at: ${line} Count: ${i}"
     fi    
   done < <(find "${relative_directory}" -maxdepth 2 -type d -print0 | while read -d '' -r dir; do echo "${dir}"; done)  
-  printf 'Directories: %s\n' "${directories[@]}"
+  #printf 'Directories: %s\n' "${directories[@]}"
 }
 
 function rename_directory() {
   parentdir="$(dirname "${1}")"
   original_directory="${1}"
   proposed_directory="${parentdir}/${2}"
-  printf "Original Direcotry: ${original_directory}\nNew Directory: ${proposed_directory}"
+  #printf "Original Direcotry: ${original_directory}\nNew Directory: ${proposed_directory}"
   if [[ "${original_directory}" != "${proposed_directory}" ]]
   then    
     sudo mv "${1}" "${parentdir}/${2}"
-    echo "Renamed folder: ${parentdir}/${2}"
-  else
-    echo "Folder name looks good to go! No changes needed"
+    #echo "Renamed folder: ${parentdir}/${2}"
+  #else
+    #echo "Folder name looks good to go! No changes needed"
   fi 
 }
 
@@ -160,27 +160,8 @@ function rename_directory() {
 function clean_files() {  
   find_directories
   find_files
-  printf "Done Changing Titles\n"
+  #echo && echo -e "Done Changing Titles\n"
 }
-
-function main() {
-  echo "0 ${args[0]}" 
-  echo "1 ${args[1]}" 
-  
-  if [[ "${#args[@]}" -le 0 ]] ; then
-    usage    
-    exit 0
-  elif [[ ${args[0]} == "install" ]] ; then
-    install_dependencies
-    
-  elif [[ ${args[0]} == "clean" ]] ; then
-    relative_directory="${args[1]}"
-    clean_files
-  fi
-}
-
-args=("$@")
-main
 
 computer_user=$(getent passwd {1000..6000} | awk -F: '{ print $1}')
 os_version=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
