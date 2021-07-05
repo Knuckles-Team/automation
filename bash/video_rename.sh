ce1b579f8b84e00cc4ee7cc8079c51f159182a60
Knucklessg1 <knucklessg1@gmail.com>
2021-07-05 18:05:16 -0500

    video rename now performs renaming for series.

--- bash/video_rename.sh
+++ bash/video_rename.sh
@@ -27,12 +27,12 @@ function detect_os(){
 
 function ubuntu_install(){
   sudo apt update
-  sudo apt install -y mkvtoolnix atomicparsley mediainfo
+  sudo apt install -y mkvtoolnix atomicparsley mediainfo rename
 }
 
 function centos_install(){
   sudo yum update -y
-  sudo yum install mkvtoolnix atomicparsley mediainfo -y
+  sudo yum install mkvtoolnix atomicparsley mediainfo rename -y
 }
 
 function install_dependencies() {
@@ -42,99 +42,95 @@ function install_dependencies() {
 }
 
 function file_rename() {
-  list=$1
-  file_type=$2
-  if [[ -n "${list}" ]]
-  then
-    for file in "${list[@]}"
-    do
-      #printf "Checking Filetype: ${file_type}\n File: ${file}\n"
-      x="${file}"
-      if [[ "${file_type}" == "mkv" ]]; then
-        y="${x%.mkv}"
-      elif [[ "${file_type}" == "mp4" ]]; then
-        y="${x%.mp4}"
-      elif [[ "${file_type}" == "webm" ]]; then
-        y="${x%.webm}"
-      fi      
-      title=${y##*/}
-      current_title=$(mediainfo "${file}" | grep -e "Movie name" | awk -F  ":" '{print $2}' | sed 's/^ *//')
-      if [[ "${title}" != "${current_title}" ]]; then
+  #set -x
+  file=$1
+  file_type=$(echo "${file}" | sed 's/^.*\.//')
+  x="${file}"
+  #echo "File Type: ${file_type} File ${file}"
+  if [[ "${file_type}" == "mkv" ]]; then
+    y="${x%.mkv}"
+  elif [[ "${file_type}" == "mp4" ]]; then
+    y="${x%.mp4}"
+  elif [[ "${file_type}" == "webm" ]]; then
+    y="${x%.webm}"
+  fi
+  title=${y##*/}
+  current_title=$(mediainfo "${file}" | grep -e "Movie name" | awk -F  ":" '{print $2}' | sed 's/^ *//')
+  if [[ "${title}" != "${current_title}" ]]; then
 #        printf "Current Title: ${current_title}\nProposed Title: ${title}\n"
 #        sleep 10
-        if [[ "${file_type}" == "mkv" ]]; then
-          mkvpropedit "${file}" -e info -s title="${title}" #> /dev/null 2>&1
-#          printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
-#          sleep 10
-        elif [[ "${file_type}" == "webm" ]]; then
-          mkvpropedit "${file}" -e info -s title="${title}" > /dev/null 2>&1
-          #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
-        elif [[ "${file_type}" == "mp4" ]]; then
-          AtomicParsley "${file}" --title "${title}" --comment "" --overWrite > /dev/null 2>&1
-          #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
-        fi
-#     else
-#       printf "Titles already the same, no need to update: ${file}\n"
-      fi    
-      # Rename Directory of Folder
-      rename_directory "${directory}" "${title}"
-    done
+    if [[ "${file_type}" == "mkv" ]]; then
+      #echo "Modifying ${file}"
+      mkvpropedit "${file}" -e info -s title="${title}" #> /dev/null 2>&1
+      #echo "Modified ${file} with mkvpropedit"
+    elif [[ "${file_type}" == "webm" ]]; then
+      #echo "Modifying ${file}"
+      mkvpropedit "${file}" -e info -s title="${title}" > /dev/null 2>&1
+      #echo "Modified ${file} with mkvpropedit"
+      #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
+    elif [[ "${file_type}" == "mp4" ]]; then
+      #echo "Modifying ${file}"
+      AtomicParsley "${file}" --title "${title}" --comment "" --overWrite > /dev/null 2>&1
+      #echo "Modified ${file} with atomicparsley"
+      #printf "Complete!\nCleaned ${file_type} Title: ${title}\n"
+    else
+      echo "No Video File found"
+    fi
   fi
 }
 
 function find_files() {
   count=0
+  all_files_list=()
   for directory in "${directories[@]}"
   do
-    mp4_list=("${directory}"/*.mp4 "${directory}"/*.MP4)
-    mkv_list=("${directory}"/*.mkv "${directory}"/*.MKV)
-    webm_list=("${directory}"/*.webm "${directory}"/*.WEBM)
-    percent_complete=$(((count/${#directories[@]})*100))
-    clear
-    if [[ -n "${mkv_list}" ]]
-    then
-      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Processing MKV Files: ${directory} \r"
-      file_rename "${mkv_list}" "mkv"
-    else
-      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Found no MKV Files: ${directory} \r"
-    fi
-    if [[ -n "${mp4_list}" ]]
-    then
-      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Processing MP4 Files: ${directory} \r"
-      file_rename "${mp4_list}" "mp4"
-    else
-      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Found no MP4 Files: ${directory} \r"
-    fi
-    if [[ -n "${webm_list}" ]]
-    then
-      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Processing WEBM Files: ${directory} \r"
-      file_rename "${webm_list}" "webm"
-    else
-      echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Found no WEBM Files: ${directory} \r"
-    fi
-    ((count++))
-    percent_complete=$(((count/${#directories[@]})*100))
-    echo -ne "Percent Complete: ${percent_complete} | Ratio: ${count}/${#directories[@]} | Completed File Renaming                                               \r"
+    readarray -d '' mkv_list < <(find "${directory}" -name "*.mkv" -print0)
+    readarray -d '' mp4_list < <(find "${directory}" -name "*.mp4" -print0)
+    readarray -d '' webm_list < <(find "${directory}" -name "*.webm" -print0)
+    all_files_list=( "${all_files_list[@]}" "${mp4_list[@]}" "${mkv_list[@]}" "${webm_list[@]}" )
   done
+
+  #echo "All Video Files Found: ${all_files_list[*]}"
+  for file in "${all_files_list[@]}"
+  do
+    ((count++))
+    percent_complete=$(((count/${#all_files_list[@]})*100))
+    file_rename "${file}"
+    echo -e "Percent Complete: ${percent_complete} | Ratio: ${count}/${#all_files_list[@]} | Processing Media Files in: ${directory}"
+  done
+  echo -e "Percent Complete: ${percent_complete} | Ratio: ${count}/${#all_files_list[@]} | Completed File Renaming"
+
+  # Rename Directory of Folder
+  if [[ "${rename_directory_flag}" == "true" ]]; then
+    for directory in "${directories[@]}"
+    do
+      echo "Renaming directory"
+      rename_directory "${directory}" "${title}"
+    done
+  else
+    echo "Skipping Renaming of Directory"
+  fi
+
 }
 
 function find_directories() {
+  # Look for directories.
   shopt -s dotglob
   shopt -s nullglob
   i=0
-  
+
   while read line
   do
     #printf "LINE: ${line}\n"
     if [ -d "${line}" ]
     then
-      directories[ $i ]="${line}" 
+      directories[ $i ]="${line}"
       #echo "Found Valid Directory: ${directories[i]} Count: ${i}"
       (( i++ ))
     #else
       #echo "Did not find a directory at: ${line} Count: ${i}"
-    fi    
-  done < <(find "${relative_directory}" -maxdepth 2 -type d -print0 | while read -d '' -r dir; do echo "${dir}"; done)  
+    fi
+  done < <(find "${relative_directory}" -maxdepth 2 -type d -print0 | while read -d '' -r dir; do echo "${dir}"; done)
   #printf 'Directories: %s\n' "${directories[@]}"
 }
 
@@ -163,6 +159,9 @@ computer_user=$(getent passwd {1000..6000} | awk -F: '{ print $1}')
 os_version=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 os_version="${os_version:1:-1}"
 architecture="$(uname -m)"
+rename_directory_flag="false"
+# To rename multiple files with pattern:
+# ${filename} | sed 's/^\[MKV\] //' | sed 's/\[Multi Subs\]//'
 
 if [ -z "$1" ]; then
   usage
@@ -183,13 +182,18 @@ while test -n "$1"; do
       exit 0
       ;;
     c | -c | --clean)
-      if [ ${2} ]; then
+      if [[ "${2}" ]]; then
         relative_directory="${2}"
+        echo "Relative Directory passed: ${relative_directory}"
         shift
       else
         echo 'ERROR: "-c | --clean" requires a non-empty option argument.'
         exit 0
       fi
+      shift
+      ;;
+    r | -r | --rename-directory)
+      rename_directory_flag="true"
       shift
       ;;
     --)# End of all options.
