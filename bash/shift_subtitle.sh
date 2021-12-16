35e2286aba3c296e06e27cb8ee65cd2991a507b9
AUdel <knucklessg1@gmail.com>
2021-12-16 16:29:43 -0600

    shift subtitles completed.

--- bash/shift_subtitle.sh
+++ bash/shift_subtitle.sh
@@ -9,22 +9,80 @@
 
 set -o errexit -o noclobber -o nounset -o pipefail
 
-date_offset="$1"
-
-shift_date() {
-    date --date="$1 $date_offset" +%T,%N | cut -c 1-12
+function usage(){
+  echo "shift_subtitle.sh -f 'subtitle.srt' -s '+3.0 seconds'"
 }
 
-while read -r line
-do
-    if [[ $line =~ ^[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\ --\>\ [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]$ ]]
-    then
-        read -r start_date separator end_date <<<"$line"
-        new_start_date="$(shift_date "$start_date")"
-        new_end_date="$(shift_date "$end_date")"
-        printf "%s %s %s\n" "$new_start_date" "$separator" "$new_end_date"
-        echo "New date"
+function shift_date() {
+  date --date="${1} ${date_offset}" +%T,%N | cut -c 1-12
+}
+
+function shift_subtitle(){
+  while read -r line
+  do
+    if [[ ${line} =~ ^[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\ --\>\ [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]$ ]]; then
+      read -r start_date separator end_date <<< "${line}"
+      new_start_date="$(shift_date "${start_date}")"
+      new_end_date="$(shift_date "${end_date}")"
+      printf "%s %s %s\n" "${new_start_date}" "${separator}" "${new_end_date}"
+      echo "New date"
     else
-        printf "%s\n" "$line"
+      printf "%s\n" "${line}"
     fi
+  done < "${subtitle_file}"
+}
+
+function generate_subtitle(){
+  shift_subtitle | tee "output-${subtitle_file}"
+  mv "${subtitle_file}" "backup-${subtitle_file}"
+  mv "output-${subtitle_file}" "${subtitle_file}"
+}
+
+if [ -z "$1" ]; then
+  usage
+  exit 0
+fi
+
+date_offset="+3.0 seconds"
+subtitle_file="subtitle.srt"
+while test -n "$1"; do
+  case "$1" in
+    h | -h | --help)
+      usage
+      exit 0
+      ;;
+    s | -s | --seconds)
+      if [ "${2}" ]; then
+        date_offset="${2}"
+        shift
+      else
+        echo 'ERROR: "-s | --seconds" requires a non-empty option argument.'
+        exit 0
+      fi
+      shift
+      ;;
+    f | -f | --file)
+      if [ "${2}" ]; then
+        subtitle_file="${2}"
+        shift
+      else
+        echo 'ERROR: "-f | --file" requires a non-empty option argument.'
+        exit 0
+      fi
+      shift
+      ;;
+    --)# End of all options.
+      shift
+      break
+      ;;
+    -?*)
+      printf 'WARNING: Unknown option (ignored): %s\n' "$1" >&2
+      ;;
+    *)
+      shift
+      break
+      ;;
+  esac
 done
+
+generate_subtitle
