44324eba50ab02e9dbfb0e2f8c728506c3908ca8
AUdel <knucklessg1@gmail.com>
2021-12-16 21:42:48 -0600

    parallelized download_series.sh

--- bash/download_series.sh
+++ bash/download_series.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
 function usage(){
-  echo "clean_series.sh -v 'video.mp4' -s 'subtitle.srt' -tv '9' -ts '+3.0 seconds'"
+  echo "download_series.sh --download-directory '~/Downloads' --file 'video_links.csv' --tv '9' -ts '+3.0 seconds'"
+  echo "CSV Format: "
+  echo "<Title>,<Video Link>,<Subtitle Link>"
 }
 
 function clean_series(){
+  index=0
   while IFS=, read -r title video_link subtitle_link
   do
-    echo "I got:$title|$video_link|$subtitle_link"
-    subtitle_file="${download_dir}/${title}.srt"
-    video_file="${download_dir}/${title}.mp4"
-    #./video_download.sh --links "${video_link}" --title "${title}" --download-directory "${download_dir}"
-    #wget --output-document "${subtitle_file}" "${subtitle_link}"
-  #  sed -i 's/WEBVTT//' "${subtitle_file}"
-  #  sed -i 's/^.*FILIMO.*$/./g' "${subtitle_file}"
-  #  sed -i 's/^.*Supervisor of Translators:.*$/./g' "${subtitle_file}"
-  #  # Trim the beginning of the video
-  #  ffmpeg -i "${video_file}" -ss "${trim_video}" -vcodec copy -acodec copy "output-${video_file}"
-  #  # Shift subtitles
-  #  shift_subtitle.sh -f "${subtitle_file}" -s "${trim_subtitle}"
-  #  # Add subtitles to video
-  #  add_subtitles.sh -s "${subtitle_file}" -v "${video_file}"
+    (
+    subtitle_files+=("${download_dir}/${title}.srt")
+    video_files+=("${download_dir}/${title}.mp4")
+    ./video_download.sh --links "${video_link}" --title "${title}" --download-directory "${download_dir}"
+    wget --output-document "${subtitle_files[${index}]}" "${subtitle_link}"
+    sed -i 's/WEBVTT//' "${subtitle_files[${index}]}"
+    sed -i 's/^.*FILIMO.*$/./g' "${subtitle_files[${index}]}"
+    sed -i 's/^.*Supervisor of Translators:.*$/./g' "${subtitle_files[${index}]}"
+    # Trim the beginning of the video
+    ffmpeg -nostdin -i "${video_files[${index}]}" -ss "${trim_video}" -vcodec copy -acodec copy "output-${video_files[${index}]}"
+    rm -f "${video_files[${index}]}"
+    mv "output-${video_files[${index}]}" "${video_files[${index}]}"
+    # Shift subtitles
+    shift_subtitle.sh -f "${subtitle_files[${index}]}" -s "${trim_subtitle}"
+    # Add subtitles to video
+    add_subtitles.sh -s "${subtitle_files[${index}]}" -v "${video_files[${index}]}"
+    ) &
+    index=${index}+1
   done < "${file}"
-  # Clean subtitles
-#  sed -i 's/ [0-9][0-9]* /\n\n&\n/g' "${subtitle_file}"
-#  sed -i 's/^ //g' "${subtitle_file}"
-#  sed -i 's/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9] /&\n/g' "${subtitle_file}"
-
-#  sed -i 's/WEBVTT//' "${subtitle_file}"
-#  sed -i 's/^.*FILIMO.*$/./g' "${subtitle_file}"
-#  sed -i 's/^.*Supervisor of Translators:.*$/./g' "${subtitle_file}"
-#  # Trim the beginning of the video
-#  ffmpeg -i "${video_file}" -ss "${trim_video}" -vcodec copy -acodec copy "output-${video_file}"
-#  # Shift subtitles
-#  shift_subtitle.sh -f "${subtitle_file}" -s "${trim_subtitle}"
-#  # Add subtitles to video
-#  add_subtitles.sh -s "${subtitle_file}" -v "${video_file}"
 }
 
 if [ -z "$1" ]; then
@@ -45,8 +38,8 @@ fi
 
 file=""
 download_dir="."
-subtitle_file="subtitle.srt"
-video_file="movie.mp4"
+subtitle_files=()
+video_files=()
 trim_video="15"
 trim_subtitle="-18.0 seconds"
 while test -n "$1"; do
@@ -75,16 +68,6 @@ while test -n "$1"; do
       fi
       shift
       ;;
-    s | -s | --subtitle-file)
-      if [ "${2}" ]; then
-        subtitle_file="${2}"
-        shift
-      else
-        echo 'ERROR: "-s | --subtitle-file" requires a non-empty option argument.'
-        exit 0
-      fi
-      shift
-      ;;
     ts | -ts | --trim-subtitle)
       if [ "${2}" ]; then
         trim_subtitle="${2}"
@@ -98,16 +81,6 @@ while test -n "$1"; do
     tv | -tv | --trim-video)
       if [ "${2}" ]; then
         trim_video="${2}"
-        shift
-      else
-        echo 'ERROR: "-v | --video-file" requires a non-empty option argument.'
-        exit 0
-      fi
-      shift
-      ;;
-    v | -v | --video-file)
-      if [ "${2}" ]; then
-        subtitle_file="${2}"
         shift
       else
         echo 'ERROR: "-v | --video-file" requires a non-empty option argument.'
