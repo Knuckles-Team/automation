e186eba8c5ffce2eb5de1597abfd7a52f966e4ec
AUdel <knucklessg1@gmail.com>
2021-12-28 18:58:56 -0600

    Added script to compile benchdnn

---
+++ bash/benchdnn_manager.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function compile_benchdnn(){
+  if [[ -d "oneDNN" ]]; then
+    echo "oneDNN exists on your filesystem."
+  else
+    git clone https://github.com/oneapi-src/oneDNN.git
+  fi
+  cd oneDNN || echo "Git Repo not cloned"
+  mkdir -p build
+  cd build || echo "build directory not created"
+  export CC=icc
+  export CXX=icpc
+  cmake .. \
+        -DDNNL_CPU_RUNTIME=DPCPP
+  make -j
+}
+
+function run_benchdnn(){
+  # https://github.com/oneapi-src/oneDNN/blob/master/tests/benchdnn/README.md
+  # ./benchdnn --DRIVER [COMMON-OPTIONS] [DRIVER-OPTIONS] PROBLEM-DESCRIPTION
+  ./benchdnn --DRIVER [COMMON-OPTIONS] [DRIVER-OPTIONS] PROBLEM-DESCRIPTION
+}
+
+compile_benchdnn
+run_benchdnn
