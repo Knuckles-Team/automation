15f39268ad9885e61c7a75e9c873568a405e166f
Knucklessg1 <knucklessg1@gmail.com>
2022-01-01 21:01:40 -0600

    Added whatsapp automation

---
+++ python/whatsapp_automation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#pip install pywhatkit
+
+import pywhatkit
+
+# Parameters: <Mobile Number with Country Code>, <Message>, <Hour>, <Minutes>
+pywhatkit.sendwhatmsg("+923019089476", "ali", 23, 5)
