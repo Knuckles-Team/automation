1c0fc3df9084a928ed4413baddb18a805cb79851
Live session user <knucklessg1@gmail.com>
2022-03-20 11:23:51 -0500

    Cockpit container

---
+++ docker/cockpit/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+apt update
+apt install cockpit cockpit-storaged cockpit-networkmanager cockpit-packagekit cockpit-ostree cockpit-machines cockpit-sosreport cockpit-kdump -y
+
+# If you already have Cockpit on your server, point your web browser to: https://ip-address-of-machine:9090
