0e48d71fa72eb9adfbd3914cda74e2119d018be5
Knucklessg1 <knucklessg1@gmail.com>
2022-04-24 13:26:59 -0500

    Base script with 90% functionality

--- powershell/theme-terminal.ps1
+++ powershell/theme-terminal.ps1
@@ -1,47 +1,44 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-function GitInstall() {
-  $appToMatch = '*Git*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Git"
-    $Path = $env:TEMP;
-    $Installer = "git_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Git already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.29.2.windows.2/Git-2.29.2.2-64-bit.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Git Installed Successfully"
-  }
-  else {
-    Write-Host "Git already installed!"
-  }
+function InstallChocolatey(){
+  Set-ExecutionPolicy AllSigned
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(‘https://chocolatey.org/install.ps1’))
+}
+function InstallGit() {
+  winget install -e --id Git.Git
 }
 
-function SevenzipInstall() {
-  $appToMatch = '*7*zip*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing 7Zip"
-    $Path = $env:TEMP;
-    $Installer = "7z_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "7zip already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "7Zip Installed Successfully"
-  }
-  else {
-    Write-Host "7Zip already installed!"
-  }
+function InstallSevenzip() {
+  Choco Install 7zip.install
+}
+
+function InstallWindowsTerminal(){
+  Choco Install microsoft-windows-terminal
+}
+
+function InstallPowerShellCore(){
+  Choco Install powershell-core
+}
+
+function InstallNeoVim(){
+  Choco Install neovim
+}
+
+function InstallGCC(){
+  Choco Install msys2
+}
+
+function InstallJQ(){
+  Choco Install jq
+}
+
+function InstallPosh(){
+  Install-Module posh-git -Scope CurrentUser -Force
+  Install-Module oh-my-posh -Scope CurrentUser -Force
+  Install-Module Terminal-Icons -Repository PSGallery -Force
+  Install-Module -Name z -Force
+  Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+  Install-Module -Name PSFzf -Scope CurrentUser -Force
 }
 
 function InstallFonts(){
@@ -61,3 +58,34 @@ function InstallFonts(){
   cp *.ttf c:\windows\fonts\
   Remove-Item "$Path\Hack.zip"
 }
+
+function CustomizeTerminal(){
+  Write-Host "Modifying Terminal Profile"
+  cd $env:TEMP
+  git clone https://github.com/craftzdog/dotfiles-public.git
+  cp $env:TEMP/dotfiles-public/.config $home
+}
+
+function InstallDependencies(){
+  InstallChocolatey
+  InstallGit
+  InstallSevenzip
+  InstallWindowsTerminal
+  InstallPowerShellCore
+  InstallNeoVim
+  InstallGCC
+  InstallFonts
+  InstallPosh
+}
+
+function ProvisionTheme(){
+  Write-Host "Provisioning Theme"
+
+}
+
+function Main(){
+  InstallDependencies
+  CustomizeTerminal
+}
+
+Main
