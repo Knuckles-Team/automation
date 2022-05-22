e0c4487763b321989d4d5e4342a8bed93ff8702b
Knucklessg1 <knucklessg1@gmail.com>
2022-05-22 18:20:20 -0500

    Reformated file to run with Chocolatey only.

--- powershell/provision.ps1
+++ powershell/provision.ps1
@@ -3,19 +3,6 @@
 # Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-function Get-InstalledApps() {
-    if ([IntPtr]::Size -eq 4) {
-        $regpath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-    }
-    else {
-        $regpath = @(
-            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-            'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-        )
-    }
-    Get-ItemProperty $regpath | .{process{if($_.DisplayName -and $_.UninstallString) { $_ } }} | Select-Object DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString | Sort-Object DisplayName
-}
-
 function InstallFeature($name){
   Write-Host "adding Windows 10 feature $name";
   Enable-WindowsOptionalFeature -Online -FeatureName $name -NoRestart
@@ -60,161 +47,15 @@ function EnableFeatures(){
   CheckWindowsFeature MicrosoftWindowsPowerShellV2Root
 }
 
-function AudacityInstall() {
-  $appToMatch = '*Audacity*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Audacity"
-    $Path = $env:TEMP;
-    $Installer = "audacity_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Audacity already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://www.fosshub.com/Audacity.html/audacity-win-3.0.0.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "--quiet" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Audacity Installed Successfully"
-  }
-  else {
-    Write-Host "Audacity already installed!"
-  }
-}
-function ChromeInstall() {
-  $appToMatch = '*Chrome*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Chrome"
-    $Path = $env:TEMP;
-    $Installer = "chrome_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Chrome already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Chrome Installed Successfully"
-  }
-  else {
-    Write-Host "Chrome already installed!"
-  }
+function InstallChocolatey(){
+  Write-Host "Installing Chocolatey"
+  Set-ExecutionPolicy AllSigned
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+  Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 }
 
-function SevenzipInstall() {
-  $appToMatch = '*7*zip*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing 7Zip"
-    $Path = $env:TEMP;
-    $Installer = "7z_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "7zip already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "7Zip Installed Successfully"
-  }
-  else {
-    Write-Host "7Zip already installed!"
-  }
-}
-
-function DockerInstall() {
-  $appToMatch = '*Docker*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Docker"
-    $Path = $env:TEMP;
-    $Installer = "docker_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Docker already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "--quiet" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Docker Installed Successfully"
-  }
-  else {
-    Write-Host "Docker already installed!"
-  }
-}
-
-function GimpInstall() {
-  $appToMatch = '*Gimp*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Gimp"
-    $Path = $env:TEMP;
-    $Installer = "gimp_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Gimp already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.22-setup.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Gimp Installed Successfully"
-  }
-  else {
-    Write-Host "Gimp already installed!"
-  }
-}
-
-function GitInstall() {
-  $appToMatch = '*Git*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Git"
-    $Path = $env:TEMP;
-    $Installer = "git_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Git already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.29.2.windows.2/Git-2.29.2.2-64-bit.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Git Installed Successfully"
-  }
-  else {
-    Write-Host "Git already installed!"
-  }
-}
-
-function NotepadPPInstall() {
-  $appToMatch = '*Notepad*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Notepad++"
-    $Path = $env:TEMP;
-    $Installer = "notepad_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Notepad++ already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.5/npp.7.9.5.Installer.x64.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Notepad++ Installed Successfully"
-  }
-  else {
-    Write-Host "Notepad++ already installed!"
-  }
-}
-
-
-function OpenSSHInstall() {
+function InstallOpenSSH() {
   # Install OpenSSH Client and Server
   Write-Host "Installing OpenSSH"
   # Find latest version
@@ -237,165 +78,30 @@ function OpenSSHInstall() {
   Write-Host "OpenSSH Installed Successfully"
 }
 
-function PythonInstall(){
-	winget install --id=Python.Python.3 -e
-    # Update Pip
-    python -m pip install --upgrade pip
-    # Install Python Dependencies
-    python -m pip install youtube-dl webarchiver subshift
-    # Remove downloaded files and directory
-    Remove-Item -r $Path\$Installer
-    Write-Host "Python Environment Installed Successfully"
-
+function InstallFonts(){
+  $Path = $env:TEMP;
+  Invoke-WebRequest "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip" -OutFile "$Path\Hack.zip";
+  Expand-Archive "$Path\Hack.zip" -DestinationPath "$Path\Hack"
+  Set-Location "$Path\Hack"
+  $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+  foreach ($file in Get-ChildItem *.ttf)
+  {
+    $fileName = $file.Name
+    if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
+      Write-Output $fileName
+      Get-ChildItem $file | ForEach-Object{ $fonts.CopyHere($_.fullname) }
+    }
+  }
+  Copy-Item *.ttf c:\windows\fonts\
+  Remove-Item "$Path\Hack.zip"
 }
 
-function PycharmInstall(){
-  $appToMatch = '*Pycharm*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Pycharm"
-    $Path = $env:TEMP;
-    $Installer = "pycharm_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "SourceTree already downloaded!"
-    }
-    else {
-      Invoke-WebRequest -Uri "https://download.jetbrains.com/python/pycharm-community-2020.2.4.exe" -OutFile $Path\$Installer;
-    }
-    $location = Get-Location
-    $tkthread_path = "$location\thread2.8.4.zip"
-    $tkthread_final_path = $(python -c "import os, sys; print(os.path.dirname(sys.executable))")
-    $tkthread_final_path = "$tkthread_final_path\tcl\tcl8.6\thread2.8.4"
-    Expand-Archive $tkthread_path -DestinationPath $tkthread_final_path
-    # Install exes
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install InstallAllUsers=1 PrependPath=1 Include_test=0" -Verb RunAs -Wait;
-    # Reload Environment Variables when installing something during script
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-    # Remove downloaded files and directory
-    Remove-Item -r $Path\$Installer
-    Write-Host "Pycharm Installed Successfully"
-  }
-  else {
-    Write-Host "Pycharm already installed!"
-  }
-}
-
-function SourceTreeInstall() {
-  $appToMatch = '*SourceTree*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing SourceTree"
-    $Path = $env:TEMP;
-    $Installer = "sourcetree_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "SourceTree already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-3.3.9.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "SourceTree Installed Successfully"
-  }
-  else {
-    Write-Host "SourceTree already installed!"
-  }
-}
-
-function SteamInstall() {
-  $appToMatch = '*Steam*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Steam"
-    $Path = $env:TEMP;
-    $Installer = "steam_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Steam already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Steam Installed Successfully"
-  }
-  else {
-    Write-Host "Steam already installed!"
-  }
-}
-
-function TransmissionInstall() {
-  $appToMatch = '*Transmission*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing Transmission"
-    $Path = $env:TEMP;
-    $Installer = "transmission-qt_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Transmission already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://github.com/transmission/transmission-releases/raw/master/transmission-3.00-x64.msi" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "Transmission Installed Successfully"
-  }
-  else {
-    Write-Host "Transmission already installed!"
-  }
-}
-
-function VLCInstall() {
-  $appToMatch = '*VLC*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    Write-Host "Installing VLC"
-    $Path = $env:TEMP;
-    $Installer = "vlc_installer.exe";
-    if (Test-Path -Path $Path\$Installer -PathType Leaf) {
-      Write-Host "Transmission already downloaded!"
-    }
-    else {
-      Invoke-WebRequest "https://ftp.osuosl.org/pub/videolan/vlc/3.0.11/win64/vlc-3.0.11-win64.exe" -OutFile $Path\$Installer;
-    }
-    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
-    Remove-Item $Path\$Installer
-    Write-Host "VLC Installed Successfully"
-  }
-  else {
-    Write-Host "VLC already installed!"
-  }
-}
-
-function WSLInstall() {
-  $appToMatch = '*Ubuntu*'
-  $result = Get-InstalledApps | Where-Object {$_.DisplayName -like $appToMatch}
-  If ($null -eq $result) {
-    # Enable Ubuntu and Fedora Linux and set Ubuntu default user to root
-    Write-Host "Enabling WSL2"
-    # Enable WSL2
-    CheckWindowsFeature Microsoft-Windows-Subsystem-Linux
-
-    # Download Ubuntu
-    $Path = $env:TEMP;
-    $Ubuntu_Installer = "Ubuntu.appx";
-    $Fedora_Installer = "Fedora.appx";
-    Invoke-WebRequest -Uri "https://aka.ms/wslubuntu2004" -OutFile $Path\$Ubuntu_Installer -UseBasicParsing;
-    Invoke-WebRequest -Uri "https://github.com/WhitewaterFoundry/WSLFedoraRemix/releases/" -OutFile $Path\$Fedora_Installer -UseBasicParsing;
-    Add-AppxPackage .\$Ubuntu_Installer
-    Add-AppxPackage .\$Fedora_Installer
-    Remove-Item $Path\$Ubuntu_Installer
-    Remove-Item $Path\$Fedora_Installer
-    ubuntu config --default-user root
-    Write-Host "WSL2 Enabled Successfully"
-  }
-  else {
-    Write-Host "WSL2 already installed!"
-  }
-}
-
-function FlushTemp(){
+function ProvisionSystem() {
+  Install-Module -Name z -Force -AllowClobber
+  Install-Module -Name PSReadLine -Force -SkipPublisherCheck
+  Install-Module -Name PSFzf -Force
+  Choco Install 7zip.install audacity audacity-lame audacity-ffmpeg battle.net curl discord docker-desktop fzf epicgameslauncher gimp git.install goggalaxy googlechrome jq k-litecodecpackfull libreoffice-fresh powershell-core msys2 neovim notepadplusplus.install microsoft-windows-terminal mpc-be nuget.commandline oh-my-posh openssl.light origin putty pycharm-edu python steam-client terminal-icons.powershell transmission virtualbox vlc vscode vcredist140 winscp.install wireshark wget wsl-ubuntu-2004 zoom -y
+  New-Item -ItemType SymbolicLink -Path (Join-Path -Path $Env:USERPROFILE -ChildPath Documents) -Name PowerShell -Target (Join-Path -Path $Env:USERPROFILE -ChildPath Documents\WindowsPowerShell)
   Remove-Item -r $env:TEMP;
 }
 
@@ -405,26 +111,13 @@ function SetStartupPrograms(){
   Copy-Item $TaskManagerShortcut -Destination $StartUpDirectory
 }
 
-#Main-function
 function Main {
-  ChromeInstall
+  InstallChocolatey
   EnableFeatures
-  SevenzipInstall
-  DockerInstall
-  AudacityInstall
-  GimpInstall
-  GitInstall
-  NotepadPPInstall
-  OpenSSHInstall
-  PythonInstall
-  PycharmInstall
-  SourceTreeInstall
-  SteamInstall
-  TransmissionInstall
-  VLCInstall
-  WSLInstall
+  InstallFonts
+  InstallOpenSSH
+  ProvisionSystem
   SetStartupPrograms
-  FlushTemp
 }
 
 Main
