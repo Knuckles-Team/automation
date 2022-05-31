e0edd1dbbca9d55348bd095ff080d0baade3ab87
Knucklessg1 <knucklessg1@gmail.com>
2022-05-31 12:10:24 -0500

    Final changes.

--- docker/automation/build-image.sh
+++ docker/automation/build-image.sh
@@ -5,10 +5,14 @@ scripts_directory="$(pwd)"
 automation_directory="$(pwd)/../../../automation"
 pushd "${automation_directory}"
 automation_directory="$(pwd)"
+cp -r "${automation_directory}" "/tmp/automation"
+rm -rf "/tmp/automation/.*"
+rm -rf "/tmp/automation/.gitignore"
+rm -rf "/tmp/automation/.gitattributes"
 popd
-tar -czvf /tmp/automation.tar.gz -C "${automation_directory}" * --overwrite || exit 1
+tar -czvf /tmp/automation.tar.gz -C "/tmp/automation" . --overwrite --exclude-vcs --exclude='.idea' || exit 1
 mv /tmp/automation.tar.gz "${scripts_directory}/automation.tar.gz"
-docker build -t automation:latest -f "$(pwd)/../../docker/automation/Dockerfile" .
-# rm -rf ./automation.tar.gz
-docker run -it automation:latest bash
-# docker run -v /mnt:/mnt -it automation:latest bash
+docker build -t automation:latest -f "${automation_directory}/docker/automation/Dockerfile" .
+rm -rf ./automation.tar.gz
+rm -rf /tmp/*
+docker run -v /mnt:/mnt -it automation:latest bash
