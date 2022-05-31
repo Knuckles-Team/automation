1bad0894a7d92abb05ae11f88da52ef74558318b
Knucklessg1 <knucklessg1@gmail.com>
2022-05-31 11:46:27 -0500

    Backup changes.

--- docker/automation/build-image.sh
+++ docker/automation/build-image.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -x
 
-yes | cp -rf ../../../automation ./automation
+scripts_directory="$(pwd)"
+automation_directory="$(pwd)/../../../automation"
+pushd "${automation_directory}"
+automation_directory="$(pwd)"
+popd
+tar -czvf /tmp/automation.tar.gz -C "${automation_directory}" * --overwrite || exit 1
+mv /tmp/automation.tar.gz "${scripts_directory}/automation.tar.gz"
 docker build -t automation:latest -f "$(pwd)/../../docker/automation/Dockerfile" .
-rm -rf ./automation
-docker run -v /mnt:/mnt -it automation:latest bash
+# rm -rf ./automation.tar.gz
+docker run -it automation:latest bash
+# docker run -v /mnt:/mnt -it automation:latest bash
