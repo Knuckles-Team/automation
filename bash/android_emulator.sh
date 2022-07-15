294899cbcece70de3777c90a294739c302fef236
Knucklessg1 <knucklessg1@gmail.com>
2022-07-15 00:45:53 -0500

    Adding android emulator.

---
+++ bash/android_emulator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#apt install libgl1-mesa-dev
+#wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+#tar -xvf android-sdk_r24.4.1-linux.tgz
+#android-sdk-linux/tools/android
+apt install adb
+snap install --devmode --beta anbox
