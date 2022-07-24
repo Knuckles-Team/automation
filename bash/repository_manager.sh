e240c8479fefdf0b7e6952b3934ec1f44ee5add7
Audel Rouhi <knucklessg1@gmail.com>
2022-07-24 14:26:57 -0500

    Delete repository_manager.sh

--- bash/repository_manager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-repo_directory="~/storage/shared/development"
-repos=("media-downloader", "automation", "subshift", "webarchiver", "genius-bot")
-pushd "${repo_directory}"
-for repo in repos; do
-  git clone https://github.com/Knucklessg1/${repo}
-done
-popd
