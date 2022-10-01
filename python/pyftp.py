d8a4b019876afd308231c6a0bb7097d269a6f7dc
Knucklessg1 <knucklessg1@gmail.com>
2022-10-01 17:57:03 -0500

    Fully working ftp that sends multiple files.

--- python/pyftp.py
+++ python/pyftp.py
@@ -17,26 +17,19 @@ class FTP:
         self.internal_ip = ""
         self.external_ip = ""
         self.generate_ips()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = None #socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.buffer_size = buffer_size
 
-    def send(self, file):
+    def set_sender_socket_configuration(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if self.type == "local":
             self.socket.bind((self.internal_ip, self.port))  # if the clients/server are on different network you shall bind to ('', port)
         else:
             self.socket.bind(("", self.port))
         self.socket.listen(5)
-        print("Initiating file sending")
-        (connection, (ip,port)) = self.socket.accept()
-        print(f'{ip} connected.')
-        open_file = open(file, "rb")
-        file_size = os.path.getsize(file)
-        file_bytes = open_file.read(file_size)
-        connection.sendall(file_bytes)
-        open_file.close()
-        print("Done sending!")
 
-    def receive(self, file):
+    def set_receiver_socket_configuration(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         attempts = 1
         while attempts <= 3:
             try:
@@ -44,14 +37,66 @@ class FTP:
                 attempts = 4
             except Exception as e:
                 if attempts == 3:
-                    print(
-                        f"Unable to connect to Sender. Max Attempts Reached. \n\tError: {e} \n\tAttempts: ({attempts})")
+                    print(f"Unable to connect to Sender. Max Attempts Reached. \n\tError: {e} \n\tAttempts: ({attempts})")
                     sys.exit(2)
                 else:
                     print(f"Unable to connect to Sender. Trying again... \n\tError: {e} \n\tAttempt: ({attempts})")
             time.sleep(6)
             attempts = attempts + 1
-        print(f"Initiating file download from: {self.sender_ip_address}")
+
+    def send_number_of_files(self, files):
+        self.set_sender_socket_configuration()
+        (connection, (ip,port)) = self.socket.accept()
+        print(f'Sending Number of Files: {len(files)}')
+        connection.send(f"{len(files)}".encode('utf-8'))
+        self.socket.close()
+
+    def receive_number_of_files(self):
+        self.set_receiver_socket_configuration()
+        total_files = self.socket.recv(self.buffer_size)
+        total_files = total_files.decode('utf-8')
+        self.socket.close()
+        print(f'Receiving Number of Files: {total_files}')
+        return int(total_files)
+
+    def send_file_name(self, file):
+        self.set_sender_socket_configuration()
+        (connection, (ip,port)) = self.socket.accept()
+        print(f'\tSending File Name: {file}')
+        connection.send(f"{file}".encode('utf-8'))
+        self.socket.close()
+
+    def receive_file_name(self):
+        self.set_receiver_socket_configuration()
+        file = self.socket.recv(self.buffer_size)
+        file = file.decode('utf-8')
+        file = os.path.basename(file)
+        original_file = file
+        self.socket.close()
+        copy = 1
+        file_name, file_extension = os.path.splitext(file)
+        while os.path.exists(file):
+            file = file_name + f"({copy})" + file_extension
+            copy = copy + 1
+        return file, original_file
+
+    def send(self, file):
+        self.send_file_name(file)
+        self.set_sender_socket_configuration()
+        (connection, (ip,port)) = self.socket.accept()
+        print(f'\tSending File Data')
+        open_file = open(file, "rb")
+        file_size = os.path.getsize(file)
+        file_bytes = open_file.read(file_size)
+        connection.sendall(file_bytes)
+        open_file.close()
+        print(f"\tSent: {file} to {ip}")
+        self.socket.close()
+
+    def receive(self):
+        file, original_file = self.receive_file_name()
+        print(f"\tReceiving: {original_file} from: {self.sender_ip_address}")
+        self.set_receiver_socket_configuration()
         open_file = open(file, "wb")
         while True:
             file_bytes = self.socket.recv(self.buffer_size)
@@ -64,7 +109,8 @@ class FTP:
                     break
         open_file.write(data)
         open_file.close()
-        print("Done receiving!")
+        print(f"\tReceived: {original_file}")
+        self.socket.close()
 
     def get_internal_ip(self):
         return self.internal_ip
@@ -98,13 +144,14 @@ def usage():
 
 def pyftp(argv):
     action = ""
+    files = []
     file = ""
     port = 59630
     sender_ip_address = ""
     type = "local"
 
     try:
-        opts, args = getopt.getopt(argv, "ha:f:i:p:t:", ["help", "action=", "file=", "ip-address=", "port=", "type="])
+        opts, args = getopt.getopt(argv, "ha:f:i:p:t:", ["help", "action=", "files=", "ip-address=", "port=", "type="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -114,8 +161,12 @@ def pyftp(argv):
             sys.exit()
         elif opt in ("-a", "--action"):
             action = arg
-        elif opt in ("-f", "--file"):
-            file = arg
+        elif opt in ("-f", "--files"):
+            files = arg.split(",")
+            files = files + arg.split(", ")
+            for file_index in range(0, len(files)):
+                files[file_index] = files[file_index].strip()
+            files = list(set(files))
         elif opt in ("-i", "--ip-address"):
             sender_ip_address = arg
         elif opt in ("-p", "--port"):
@@ -137,14 +188,21 @@ def pyftp(argv):
                 sys.exit(2)
 
     if action == "send":
-        if not os.path.exists(file):
+        print(f"Files to Send: {files}")
+        if not os.path.exists(files[0]):
             print("File not found")
             usage()
             sys.exit(2)
         sender = FTP(type=type, port=port)
         print(f"Sender Internal IP Address: {sender.get_internal_ip()}\n"
               f"Sender External IP Address: {sender.get_external_ip()}")
-        sender.send(file)
+        sender.send_number_of_files(files)
+        file_index = 0
+        for file in files:
+            sender.send(file)
+            print(f"\tSent file: {file_index+1} out of {len(files)}")
+            file_index = file_index + 1
+        print(f"{len(files)} Files Sent!")
 
     elif action == "receive":
         if sender_ip_address == "":
@@ -152,10 +210,11 @@ def pyftp(argv):
             usage()
             sys.exit(2)
         receiver = FTP(sender_ip_address=sender_ip_address, port=port)
-        print(f"Reciever Internal IP Address: {receiver.get_internal_ip()}\n"
-              f"Reciever External IP Address: {receiver.get_external_ip()}")
-        receiver.receive(file)
-
+        total_files = receiver.receive_number_of_files()
+        for file_index in range(0, total_files):
+            receiver.receive()
+            print(f"\tReceived file: {file_index+1} out of {total_files}")
+        print(f"{total_files} Files Received!")
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
