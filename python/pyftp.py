440cde671ff92e97c28625586ffdc176f70ed2b4
Knucklessg1 <knucklessg1@gmail.com>
2022-10-01 16:06:36 -0500

    Removed action.

--- python/pyftp.py
+++ python/pyftp.py
@@ -10,8 +10,7 @@ import time
 
 
 class FTP:
-    def __init__(self, sender_ip_address=None, port=12345):
-        self.action = action
+    def __init__(self, sender_ip_address=None, port=12345):\
         self.sender_ip_address = sender_ip_address
         self.port = port
         self.internal_ip = ""
