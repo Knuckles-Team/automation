6d71c5e7ca3cadd031b69a2d3f5ee38eb4a298c5
Knucklessg1 <knucklessg1@gmail.com>
2022-10-11 21:12:59 -0500

    Added aimbot

--- python/gui_automation.py
+++ python/gui_automation.py
@@ -1,6 +1,9 @@
 import pyautogui
+import pynput
+import keyboard
 import platform
 import time
+pyautogui.PAUSE = 0
 
 # Moves to coordinate
 #pyautogui.moveTo(x=500, y=500, duration=3)
@@ -33,6 +36,93 @@ import time
 #pyautogui.hotkey('ctrlleft', 'alt', 'delete')
 #pyautogui.hotkey('enter')
 
+# Get coordinates
+# pyautogui.displayMousePosition()
+def aim_booster():
+    time.sleep(1)
+    #while keyboard.is_pressed('q') == False:
+    print("Starting aimbooster")
+    game_coordinates = [738, 246]  # game_coordinates[0], game_coordinates[1]
+    pixel_margin_of_error = 10
+    while True:
+        pic = pyautogui.screenshot(region=(game_coordinates[0], game_coordinates[1], game_coordinates[0] + 596, game_coordinates[1] + 418))
+        width, height = pic.size
+        previous_point = [0, 0]
+        for x in range(0, width, 5):
+            for y in range(0, height, 5):
+                r, g, b = pic.getpixel((x, y))
+                #print(f"Scanning: X {x + 748} Y {y + 440}")
+                #print(f"Previous Hit: {previous_hit}")
+                if b == 195 and r == 255 and g == 219:
+                    print(f"FOUND AT: X {x + game_coordinates[0]} Y {y + game_coordinates[1]}")
+                    #pyautogui.click(x + game_coordinates[0], y + game_coordinates[1], duration=0.05)
+                    #points.append([x + game_coordinates[0], y + game_coordinates[1]])
+                    point = [x + game_coordinates[0], y + game_coordinates[1]]
+
+                    if (point[0] - pixel_margin_of_error) > previous_point[0] or previous_point[0] > (point[0] + pixel_margin_of_error)\
+                            and (point[1] - pixel_margin_of_error) > previous_point[1] or previous_point[1] > (point[1] + pixel_margin_of_error):
+                        print(f"Clicking Point: {point}")
+                        pyautogui.click(point[0], point[1], duration=0.02)
+                    previous_point = point
+
+
+        # for x in range(0, width, 5):
+        #     for y in range(0, height, 5):
+        #         r, g, b = pic.getpixel((x, y))
+        #         #print(f"Scanning: X {x + 748} Y {y + 440}")
+        #         #print(f"Previous Hit: {previous_hit}")
+        #         if b == 195 and r == 255 and g == 219:
+        #             print(f"FOUND AT: X {x + game_coordinates[0]} Y {y + game_coordinates[1]}")
+        #             #pyautogui.click(x + game_coordinates[0], y + game_coordinates[1], duration=0.05)
+        #             points.append([x + game_coordinates[0], y + game_coordinates[1]])
+        #
+        # res_list = []
+        # previous_point = [0, 0]
+        # for i in range(len(points)):
+        #     if points[i] not in points[i + 1:] and (points[i][0] - pixel_margin_of_error) > previous_point[0] or previous_point[0] > (points[i][0] + pixel_margin_of_error)\
+        #             and (points[i][1] - pixel_margin_of_error) > previous_point[1] or previous_point[1] > (points[i][1] + pixel_margin_of_error):
+        #         print(f"ADDED POINT: {points[i]}")
+        #         res_list.append(points[i])
+        #     previous_point = points[i]
+        #
+        # points = res_list
+        # for point in points:
+        #     print(f"Clicking Point: {point}")
+        #     pyautogui.click(point[0], point[1], duration=0.02)
+
+
+        #             for previous_hit in previous_hits:
+        #                 if (x + game_coordinates[0] - 20) < previous_hit[0] < (x + game_coordinates[0] + 20) and (y + game_coordinates[1] - 20) < previous_hit[1] < (y + game_coordinates[1] + 20):
+        #                     #points.append([x + game_coordinates[0], y + game_coordinates[1]])
+        #                     duplicates = True
+        #                     # mouse.position = (x + 748, y + 440)
+        #                     # mouse.click(pynput.mouse.Button['left'])
+        #                     #pyautogui.click(x + 748, y + 440, duration=0)
+        #                     #time.sleep(0.05)
+        #                     #break
+        #                     print(f"Duplicate point found, OLD {previous_hit} NEW: {x + game_coordinates[0], y + game_coordinates[1]}")
+        #                 if not duplicates:
+        #                     print(f"FOUND INSIDE AT: X {x + game_coordinates[0]} Y {y + game_coordinates[1]}")
+        #                     points.append([x + game_coordinates[0], y + game_coordinates[1]])
+        #
+        #         if len(points) > 0:
+        #             previous_hits = points
+        #
+        #     # if flag == 1:
+        #     #     break
+        #
+        # print(f"ALL POINTS: {points} LEN {len(points)}")
+        # cap = 0
+        # for point in points:
+        #     # print(f"Clicking Point: {point}")
+        #     # mouse.position = (point[0], point[1])
+        #     # mouse.click(pynput.mouse.Button['left'])
+        #     pyautogui.click(point[0], point[1], duration=0)
+        #     # if cap == 5:
+        #     #     break
+        #     # else:
+        #     #     cap = cap + 1
+
 def open_paint():
     print("Opening paint")
     if "ubuntu" in str(version).lower():
@@ -43,7 +133,7 @@ def open_paint():
         time.sleep(1)
         pyautogui.hotkey('enter')
         time.sleep(2)
-        pyautogui.click(100, 100, duration=.2)
+        pyautogui.click(100, 100, duration=.01)
         time.sleep(1)
         pyautogui.click(100, 120, duration=.3)
         time.sleep(1)
@@ -87,11 +177,18 @@ def logout_user():
         time.sleep(2)
         pyautogui.hotkey('enter')
 
+def click_pynput(x, y, button):
+    mouse.position = (x, y)
+    button = pynput.mouse.Button['left'] if button=='left' else pynput.mouse.Button['right']
+    mouse.click(button)
 
+
+mouse = pynput.mouse.Controller()
 system = platform.system()
 release = platform.release()
 version = platform.version()
 
 print(f"System: {system}\nRelease: {release}\nVersion: {version}\nScreen Size: {pyautogui.size()}")
-open_paint()
+aim_booster()
+#open_paint()
 
