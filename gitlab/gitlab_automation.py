36719c870a4d77ad8d9924ba9f424d17310fde03
Knucklessg1 <knucklessg1@gmail.com>
2022-10-30 23:29:16 -0500

    Updated README.md

--- gitlab/gitlab_automation.py
+++ gitlab/gitlab_automation.py
@@ -3,7 +3,8 @@
 import gitlab_api
 
 token = ";aslkdfj;alskjdf;alksjdf;alksjd"
-client = gitlab_api.Api(token=token)
+gitlab_url = "<GITLAB_URL>"
+client = gitlab_api.Api(url=gitlab_url, token=token)
 
 print(client.get_users())
 
