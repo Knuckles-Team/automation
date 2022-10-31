2f9b0dfc628ebc96f1d864ae62b095be8cdac77f
Knucklessg1 <knucklessg1@gmail.com>
2022-10-31 00:43:41 -0500

    Added more functions.

--- gitlab/gitlab_automation.py
+++ gitlab/gitlab_automation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 import gitlab_api
+from pprint import pprint
 
 token = "glpat-JMKXyGxqzn3BFjrgrfsc"
-gitlab_url = "http://localhost:8080/"
+gitlab_url = "http://localhost:8080/api/v4"
 client = gitlab_api.Api(url=gitlab_url, token=token)
 
-#print(client.get_users())
+pprint(f"Users: {client.get_users()}\n\n")
 
+pprint(f"Projects: {client.get_projects()}\n\n")
+
+response = client.get_runners(runner_type='instance_type', all_runners=True)
+pprint(f"Runners: {response}")
