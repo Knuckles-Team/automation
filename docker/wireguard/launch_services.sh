f24fbd0a03efb1f26b8925f408f72e80c225a7b8
Knucklessg1 <knucklessg1@gmail.com>
2022-11-12 11:05:44 -0600

    Updated to use workdir for data.

--- docker/wireguard/launch_services.sh
+++ docker/wireguard/launch_services.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export WORKDIR=/services
+export WORKDIR="/services"
+export SERVERURL="192.168.1.XXX"
+
 mkdir -p ${WORKDIR}/wireguard
 mkdir -p ${WORKDIR}/ddclient
 
 sudo ufw allow 51820/udp
 
 echo "examplepassword" | tee ./data/wireguard/password.txt
-
-export SERVERURL="192.168.1.XXX"
 
 sudo docker-compose up -d
