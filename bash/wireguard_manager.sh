5192f18f1041e953bcd2bc9961f443a4a6769b7d
Audel Rouhi <knucklessg1@gmail.com>
2022-11-11 23:32:14 -0600

    Create wireguard_manager.sh

---
+++ bash/wireguard_manager.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt install ddclient -y
+
+echo "
+# /etc/ddclient.conf
+syslog=yes              # log the output to syslog
+mail=root               # send email notifications to root
+mail-failure=root       # send email when failed
+ssl=yes                 # use ssl when updating IP
+use=web, web=dynamicdns.park-your-domain.com/getip # look up external IP from this URL
+protocol=namecheap
+server=dynamicdns.park-your-domain.com
+login=your-domain.com
+password=your-ddns-password
+@,*    # wildcard, to update all subdomains, like a.your-domain.com, b.your-domain.com
+" | tee /etc/ddclient.conf
+
+sudo sed -i 's/run_daemon="false"/run_daemon="true"/' /etc/default/ddclient
+sudo sed -i 's/run_ipup="true"/run_ipup="false"/' /etc/default/ddclient
+sudo systemctl restart ddclient
+sudo systemctl status ddclient
+sudo systemctl enable ddclient
