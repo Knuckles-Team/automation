17ddbca671b1492e76eb91ee8a017f63d8d44b2f
Knucklessg1 <knucklessg1@gmail.com>
2022-11-26 11:15:55 -0600

    Use correct username.

--- docker/github_runner/start.sh
+++ docker/github_runner/start.sh
@@ -5,7 +5,7 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner || mkdir -p /home/docker/actions-runner && cd /home/docker/actions-runner || echo "Error: Unable to create or navigate into /home/docker/actions-runner"
+cd /home/dockercli/actions-runner || mkdir -p /home/dockercli/actions-runner && cd /home/dockercli/actions-runner || echo "Error: Unable to create or navigate into /home/dockercli/actions-runner"
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
