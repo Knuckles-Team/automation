0fd5bd963d54fcd03387d706e1d615db1b0fb6c7
Knucklessg1 <knucklessg1@gmail.com>
2022-11-28 20:57:16 -0600

    Reuse dockercli user.

--- docker/github_runner/start.sh
+++ docker/github_runner/start.sh
@@ -3,8 +3,6 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-chmod 666 /var/run/docker.sock
-
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/dockercli/actions-runner || mkdir -p /home/dockercli/actions-runner && cd /home/dockercli/actions-runner || echo "Error: Unable to create or navigate into /home/dockercli/actions-runner"
